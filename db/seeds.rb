2cfc7aa4626625a6fcc97d864f5f952244e4c7a0
taylorsieling <taylorasieling@gmail.com>
2023-09-21 21:23:10 -0500

    postgres configurations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+tlt = Litter.create([{ theme: "The Locked Tomb" }, { intake_type: "Stray - Animal Control" }])
